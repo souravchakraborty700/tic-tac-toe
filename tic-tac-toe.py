5d57fc58a7067360540ce24e2f9cb1140ce6ff59
Sourav Chakraborty <souravchakraborty700@gmail.com>
2021-10-25 11:46:51 +0530

    commited final project

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,163 +1,138 @@
-board = [0,1,2,3,4,5,6,7,8,9]
-
 def display_board(board):
-    print(board[1],'|',board[2],'|',board[3])
-    print(board[4],'|',board[5],'|',board[6])
-    print(board[7],'|',board[8],'|',board[9])
+    
+    print('    |   |')
+    print('  '+  board[7]  +' | '+board[8]+' | '+board[9])
+    print('    |   |')
+    print('--------------')
+    print('    |   |')
+    print('  ' + board[4]+' | '+board[5]+' | '+board[6])
+    print('    |   |')
+    print('--------------')
+    print('    |   |')
+    print('  '+ board[1]+' | '+board[2]+' | '+board[3])
+    print('    |   |')
 
-#Game win or loss
 
-def game_result():
-    if board[1] == board[2] == board[3] == "X":
-        print("X won the game")
-    elif board[4] == board[5] == board[6] == "X":
-        print("X won the game")
-    elif board[7] == board[8] == board[9] == "X":
-        print("X won the game")
-    elif board[1] == board[5] == board[9] == "X":
-        print("X won the game")
-    elif board[3] == board[5] == board[7] == "X":
-        print("X won the game")
-    elif board[1] == board[4] == board[7] == "X":
-        print("X won the game")
-    elif board[2] == board[5] == board[8] == "X":
-        print("X won the game")
-    elif board[3] == board[6] == board[9] == "X":
-        print("X won the game")
-    elif board[4] == board[5] == board[6] == "O":
-        print("O won the game")
-    elif board[7] == board[8] == board[9] == "O":
-        print("O won the game")
-    elif board[1] == board[5] == board[9] == "O":
-        print("O won the game")
-    elif board[3] == board[5] == board[7] == "O":
-        print("O won the game")
-    elif board[1] == board[4] == board[7] == "O":
-        print("O won the game")
-    elif board[2] == board[5] == board[8] == "O":
-        print("O won the game")
-    elif board[3] == board[6] == board[9] == "O":
-        print("O won the game")
-    elif board[1] == board[2] == board[3] == "O":
-        print("O won the game")
+
+def player_input():
+    marker = ''
+
+    while not (marker == 'X' or marker == 'O'):
+        marker = input("Player 1: Do you want to be X or O?").upper()
+
+    if marker == 'X':
+        return ('X','O')
     else:
-        pass
+        return ('O',"X")
+
+def place_marker(board, marker, position):
+    board[position] = marker
+
+def win_check(board, mark):
+
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
+    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
+    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
+    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
+    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
+    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
+    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
+    (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
 
 
 
-#Position Choice
+import random
+def choose_first():
+    if random.randint(0,1) == 0:
+        return("Player 2")
+    else:
+        return("Player 1")
 
-def position_choice():
-    choice = input("Where do you want to place? (1-9)? ")
-    return int(choice)
+# Function that indicates if a space is freely available:
 
-
-
-
-#Func for taking user input and placing on the board
-def placing_input(board, position):
-    marker_input = input("Choose your marker 'X' or 'O': ")
-
-    board[position]= marker_input
-
-    return board
+def space_check(board, position):
+    return board[position] == ' '
 
 
+def full_board_check(board):
+    for i in range(1,10):
+        if space_check(board, i):
+            return False
+    return True
+
+def player_choice(board):
+    position = 0
+    while position not in (1,2,3,4,5,6,7,8,9) or not space_check(board, position):
+        position = int(input("What is your next move? (1-9): "))
+
+    return position
+
+def replay():
+
+    return input("Do you want to replay again? Enter Yes or No: ").lower().startswith('y')
+
+#Logic Setup:
 
 
 
+print("Welcome to Tic Tac Toe")
 
 
-#logic
+while True:
 
-#1st Round
+    theBoard = [" "] * 10
+    player_1_marker, player_2_marker = player_input()
+    turn = choose_first()
+    print(turn + " will go first")
 
-display_board(board)
+    play_game = input("Do you want to play? Enter Yes or No.")
+
+    if play_game.lower()[0] == 'y':
+        game_on = True
+    else:
+        game_on = False
+
+    while game_on == True:
+        
+        if turn == 'Player 1':
+            display_board(theBoard)
+            position = player_choice(theBoard)
+            place_marker(theBoard, player_1_marker, position)
+            
+            
+            if win_check(theBoard, player_1_marker):
+                display_board(theBoard)
+                print("Player 1 has won!")
+                game_on = False
+            else:
+                if full_board_check(theBoard):
+                    display_board(theBoard)
+                    print("Game Drawn!")
+                    break
+                else:
+                    turn = "Player 2"
+        else:
+            display_board(theBoard)
+            position = player_choice(theBoard)
+            place_marker(theBoard, player_2_marker, position)
+            win_check(theBoard, player_2_marker)
+            if win_check(theBoard, player_2_marker):
+                display_board(theBoard)
+                print("Player 2 has won!")
+                game_on = False
+            else:
+                if full_board_check(theBoard):
+                    print("Game Drawn!")
+                    break
+                else:
+                    turn = "Player 1"
+            
+    if not replay():
+        break
+        
 
 
-position = position_choice()
-
-placing_input(board, position)
-
-display_board(board)  
-
-game_result()
-
-#2nd round
-
-position = position_choice()
-
-placing_input(board, position)
-
-display_board(board)
-
-game_result()
-
-#3rd round
-
-position = position_choice()
-
-placing_input(board, position)
-
-display_board(board) 
-
-game_result()
-
-#4th round
-position = position_choice()
-
-placing_input(board, position)
-
-display_board(board)
-
-game_result()
-
-#5th round
-position = position_choice()
-
-placing_input(board, position)
-
-display_board(board)
-
-game_result()
-#6th round
-position = position_choice()
-
-placing_input(board, position)
-
-display_board(board)
-
-game_result()
-#7th round
-position = position_choice()
-
-placing_input(board, position)
-
-display_board(board)
-
-game_result()
-
-#8th round
-position = position_choice()
-
-placing_input(board, position)
-
-display_board(board)
-
-game_result()
-
-#9th round
-
-position = position_choice()
-
-placing_input(board, position)
-
-display_board(board)
-
-#Result out
-
-game_result()
-
+            
 
 
 
